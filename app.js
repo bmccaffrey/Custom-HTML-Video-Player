a928df7dc1e10db0a9109f1d05a9b393324d2c40
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-21 15:50:16 -0500

    declare and define variable for Progress Fill

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 var video = document.querySelector('video');
 var controls = document.querySelector('.player__controls');
 var progress = document.querySelector('.progress');
+var progressFill = document.querySelector('.progress__filled');
