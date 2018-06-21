de5ae4fced2a9eca4e5b465d0a8db25ccbc2701b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-21 15:53:13 -0500

    declare and define variable for Forward Button

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ var play = document.querySelector('.player__button');
 var volume = document.querySelector('[name="volume"]');
 var rate = document.querySelector('[name="playbackRate"]');
 var rewind = document.querySelector('[data-skip ="-10"]');
+var forward = document.querySelector('[data-skip="25"]');
