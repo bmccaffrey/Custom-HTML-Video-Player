596a28d268f011eebda05ec098b31b817962d18a
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-21 15:50:56 -0500

    declare and define variable for Play Button

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ var video = document.querySelector('video');
 var controls = document.querySelector('.player__controls');
 var progress = document.querySelector('.progress');
 var progressFill = document.querySelector('.progress__filled');
+var play = document.querySelector('.player__button');
