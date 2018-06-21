f92fb61269a9c446338d77f79bbd1a3f9cc5c479
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-21 15:51:31 -0500

    declare and define variable for Volume Slider

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ var controls = document.querySelector('.player__controls');
 var progress = document.querySelector('.progress');
 var progressFill = document.querySelector('.progress__filled');
 var play = document.querySelector('.player__button');
+var volume = document.querySelector('[name="volume"]');
